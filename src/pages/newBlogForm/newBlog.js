2b69c9b12efced56244ec4965a7dda56d89b47b0
David Rise Knotten <david_knotten@hotmail.no>
2021-04-04 16:38:00 +0200

    Added user feedback on adding blog

--- src/pages/newBlogForm/newBlog.js
+++ src/pages/newBlogForm/newBlog.js
@@ -3,12 +3,13 @@ import {useDispatch} from "react-redux";
 import {addBlog} from "../../store/actions/blogActions";
 
 export default function NewBlog() {
-    const [image, setImage] = useState("")
+    const [image, setImage] = useState("");
     const [poster, setPoster] = useState("");
-    const [title, setTitle] = useState("")
-    const [summary, setSummary] = useState("")
-    const [mainPost, setMainPost] = useState("")
+    const [title, setTitle] = useState("");
+    const [summary, setSummary] = useState("");
+    const [mainPost, setMainPost] = useState("");
     const dispatch = useDispatch();
+    const [addedBlog, setAddedBlog] = useState(false);
 
 
 
@@ -22,60 +23,73 @@ export default function NewBlog() {
         }
 
         dispatch(addBlog(blog));
+        setAddedBlog(true);
+
+        setImage("");
+        setPoster("");
+        setTitle("");
+        setSummary("");
+        setMainPost("");
+
+        //TODO make addedblog untrue after a second asynchronously
+        setAddedBlog(false);
     }
 
     return (
-        <form>
-            <div className="form-group">
-                <label>Image</label>
-                <input type="text"
-                       className="form-control"
-                       placeholder="Image URL"
-                       value={image}
-                       onChange={(e) => setImage(e.target.value)}
+        <div>
+            {addedBlog ? (<p id="blog-added">Successfully added blog</p>) : null}
+            <form>
+                <div className="form-group">
+                    <label>Image</label>
+                    <input type="text"
+                           className="form-control"
+                           placeholder="Image URL"
+                           value={image}
+                           onChange={(e) => setImage(e.target.value)}
 
-                />
-            </div>
-            <div className="form-group">
-                <label>Poster</label>
-                <input type="text"
-                       className="form-control"
-                       placeholder="Name of poster"
-                       value={poster}
-                       onChange={(e) => setPoster(e.target.value)}
-                />
-            </div>
-            <div className="form-group">
-                <label>Title</label>
-                <input type="text"
-                       className="form-control"
-                       placeholder="Title of post"
-                       value={title}
-                       onChange={(e) => setTitle(e.target.value)}
-                />
-            </div>
-            <div className="form-group">
-                <label>Summary</label>
-                <input type="text"
-                       className="form-control"
-                       placeholder="Summary/undertitle of post"
-                       value={summary}
-                       onChange={(e) => setSummary(e.target.value)}
-                />
-            </div>
-            <div className="form-group">
-                <label>Main Post</label>
-                <textarea type="text"
-                       className="form-control"
-                       placeholder="Main post"
-                       value={mainPost}
-                       onChange={(e) => setMainPost(e.target.value)}
-                />
-            </div>
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Poster</label>
+                    <input type="text"
+                           className="form-control"
+                           placeholder="Name of poster"
+                           value={poster}
+                           onChange={(e) => setPoster(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Title</label>
+                    <input type="text"
+                           className="form-control"
+                           placeholder="Title of post"
+                           value={title}
+                           onChange={(e) => setTitle(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Summary</label>
+                    <input type="text"
+                           className="form-control"
+                           placeholder="Summary/undertitle of post"
+                           value={summary}
+                           onChange={(e) => setSummary(e.target.value)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Main Post</label>
+                    <textarea type="text"
+                              className="form-control"
+                              placeholder="Main post"
+                              value={mainPost}
+                              onChange={(e) => setMainPost(e.target.value)}
+                    />
+                </div>
 
-            <div className="btn btn-primary"
-                    onClick={handleClick}
-            >Submit Post</div>
-        </form>
+                <div className="btn btn-primary"
+                     onClick={handleClick}
+                >Submit Post</div>
+            </form>
+        </div>
     )
 }
